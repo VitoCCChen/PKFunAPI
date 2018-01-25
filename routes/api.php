bdf50f448706f27febad2bdb0b3da5b2d2a7ad90
naughtyjoint <wateleee@gmail.com>
2018-01-25 17:49:35 +0800

    修正Route API路徑

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/getEpisode', 'EpisodeController@getEpisode');
+
+Route::get('/getProgram', 'ProgramController@getProgram');
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/getEpisode', 'EpisodeController@getEpisode');
-
-Route::get('/getProgram', 'ProgramController@getProgram');
